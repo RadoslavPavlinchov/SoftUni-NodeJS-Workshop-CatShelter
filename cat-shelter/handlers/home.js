5a27188653a5032886e26d3088a8dc18e7b2ec33
Radoslav Pavlinchov <radoslav.pavlinchov@gmail.com>
2020-05-23 19:45:22 +0300

    Fixed the 404.page, did not display properly

--- cat-shelter/handlers/home.js
+++ cat-shelter/handlers/home.js
@@ -13,14 +13,15 @@ module.exports = (req, res) => {
         fs.readFile(filePath, (err, data) => {
             if (err) {
                 if (err.code == 'ENOENT') {
-                    fs.readFile(path.join(__dirname, '/views', '404.html'), (err, data) => {
+                    fs.readFile(path.normalize(path.join(__dirname, '../views', '404.html')), (err, data) => {
                         res.writeHead(200, { 'Content-Type': 'text/html' });
                         res.end(data, 'utf-8');
                     });
                 } else {
                     // Some server error
-                    res.writeHead(500);
-                    res.end(`Server error: ${err.code}`);
+                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                    res.write(`Server error: ${err.code}`)
+                    res.end();
                 }
             } else {
                 res.writeHead(200, { 'Content-Type': 'text/html' });
