3792bb46d99c09f131b684867be9114b1c1890a4
Radoslav Pavlinchov <radoslav.pavlinchov@gmail.com>
2020-05-31 17:50:44 +0300

    Created the cubeModel and tested all methods

--- cubes/index.js
+++ cubes/index.js
@@ -1,10 +1,24 @@
 const env = process.env.NODE_ENV || 'development';
 global.__basedir = __dirname;
 
-const config = require('./config/config')[env];
-const app = require('express')();
+const cubeModel = require('./models/cube');
 
-require('./config/express')(app);
-require('./config/routes')(app);
+// cubeModel.create({ name: 'test', description: 'test test'})
+//     .then(createdCube => {
+//         console.log(createdCube);
+//         return cubeModel.delete(createdCube.id);
+//     }).then((deletedCube) => {
+//         console.log('deleted cube', deletedCube);
+//         console.log('The cube has been deleted');
+//     })
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+cubeModel.update('bf494eb8-9bb3-4791-bf33-4121ba753e3d', { name: 'New name'})
+    .then((updated) => console.log(updated));
+
+// const config = require('./config/config')[env];
+// const app = require('express')();
+
+// require('./config/express')(app);
+// require('./config/routes')(app);
+
+// app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
