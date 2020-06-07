708a1a35271d29d4379b504ee5ea126b100bd652
Radoslav Pavlinchov <radoslav.pavlinchov@gmail.com>
2020-06-07 15:36:58 +0300

    Cretead cube and accessories controllers

---
+++ cubes/controllers/cubes.js
@@ -0,0 +1,33 @@
+const Cube = require('../models/cube')
+
+const getAllCubes = async () => {
+  const cubes = await Cube.find().lean()
+  return cubes
+}
+
+const getCube = async (id) => {
+  const cube = await Cube.findById(id).lean()
+
+  return cube
+}
+
+const updateCube = async (cubeId, accessoryId) => {
+  await Cube.findByIdAndUpdate(cubeId, {
+    $addToSet: {
+      accessories: [accessoryId]
+    }
+  })
+}
+
+const getCubeWithAccessories = async (id) => {
+  const cube = await Cube.findById(id).populate('accessories').lean()
+
+  return cube
+}
+
+module.exports = {
+  getAllCubes,
+  getCube,
+  updateCube,
+  getCubeWithAccessories
+}
